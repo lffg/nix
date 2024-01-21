8853d62a61319221dcaed04570c847155d5daf6e
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-21 00:08:21 -0300

    Join nixos system configuration under configuration.nix

--- hosts/hebra/configuration.nix
+++ hosts/hebra/configuration.nix
@@ -5,19 +5,101 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ./system.nix
-    ./locale.nix
-    # ./gnome.nix
     ./vm.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-25.9.0" # goddamn logseq
-  ];
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "electron-25.9.0" # goddamn logseq
+    ];
+  };
 
   nix = {
     package = pkgs.nix;
-    settings.experimental-features = ["nix-command" "flakes"];
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+
+  users.users = {
+    luiz = {
+      isNormalUser = true;
+      description = "Luiz Felipe";
+      extraGroups = ["networkmanager" "wheel"];
+      packages = [];
+      shell = pkgs.zsh;
+    };
+  };
+
+  programs.zsh.enable = true;
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+  networking = {
+    hostName = "hebra";
+    networkmanager.enable = true;
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
+
+  # Just enough packages to get stated; all other packages
+  # are installed through home-manager.
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
+  time.timeZone = "America/Sao_Paulo";
+
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_CTYPE = "pt_BR.UTF-8"; # Fix cedilla in us-intl.
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
   };
 }
