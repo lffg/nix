3c7856935585b79278692b43d4978f32f96ac373
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-24 21:28:25 -0300

    Install openssl and its pkg-config globally

--- home/hebra/pkgs.nix
+++ home/hebra/pkgs.nix
@@ -2,10 +2,22 @@
   pkgs,
   inputs,
   ...
-}: {
+}: let
+  inherit (pkgs.lib.strings) concatStringsSep;
+in {
   home.packages = with pkgs; [
     # Command-line tools
     pulseaudio
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast
+
+    # Make openssl visible throughout the system
+    openssl
+    pkg-config
   ];
+
+  home.sessionVariables = {
+    PKG_CONFIG_PATH = concatStringsSep ":" (with pkgs; [
+      "${openssl.dev}/lib/pkgconfig"
+    ]);
+  };
 }
