c2aa4e21a5fb9d0cf706d254289c2f556858a2d2
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-01-27 11:42:30 -0300

    Add podman

--- hosts/hebra/vm.nix
+++ hosts/hebra/vm.nix
@@ -3,7 +3,17 @@
   lib,
   vars,
   ...
-}:
-lib.mkIf (options ? virtualisation.memorySize) {
-  users.users.${vars.user.name}.password = "test";
+}: {
+  imports = [
+    (lib.mkIf (options ? virtualisation.memorySize) {
+      users.users.${vars.user.name}.password = "test";
+    })
+  ];
+
+  virtualisation = {
+    podman = {
+      enable = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
 }
