31c5a41fa6f7bd5706ce8ebacee046a6dd124283
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-02-17 23:24:52 -0300

    Add podman on darwin

--- home/shared/darwin/pkgs.nix
+++ home/shared/darwin/pkgs.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  pkgs-unstable,
+  ...
+}: {
   home.packages = let
     frameworks = with pkgs.darwin.apple_sdk.frameworks; [
       CoreFoundation
@@ -7,6 +11,9 @@
     ];
 
     packages = with pkgs; [
+      pkgs-unstable.podman
+      pkgs-unstable.qemu
+
       # Don't ask me why this worked
       llvmPackages.clang-unwrapped
       llvmPackages.bintools
