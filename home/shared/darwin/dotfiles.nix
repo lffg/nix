9fd3e63b442a856ddedec09e04aabc2333282357
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-02-17 22:24:56 -0300

    Disable last login message on darwin

--- home/shared/darwin/dotfiles.nix
+++ home/shared/darwin/dotfiles.nix
@@ -1,4 +1,11 @@
 {...}: {
-  # https://superuser.com/q/1106832
-  home.file.".bash_sessions_disable".text = "";
+  home.file = {
+    # Disable session restoration funcionality.
+    # See <https://superuser.com/q/1106832>
+    ".bash_sessions_disable".text = "";
+
+    # Disable last login message on start.
+    # See <https://stackoverflow.com/q/15769615>
+    ".hushlogin".text = "";
+  };
 }
