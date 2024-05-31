8f4de8f4f1a1d9643cf40a663560075964d3d259
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2024-05-31 17:07:07 -0300

    fix rust env

--- home/programs/rust/default.nix
+++ home/programs/rust/default.nix
@@ -1,8 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
     rustup
-    gcc
+    llvmPackages.clang
+    pkg-config
+    libiconv
   ];
+
+  home.sessionVariables = let
+    inherit (lib.strings) makeLibraryPath;
+  in {
+    LIBRARY_PATH = makeLibraryPath (with pkgs; [
+      libiconv
+    ]);
+  };
 
   home.file.".cargo/config.toml".text = ''
     [target.x86_64-unknown-linux-gnu]
